72449df98e3c15f1b8826b268e7a76dbc6254e6e
natephil <41304546+natephil@users.noreply.github.com>
2019-06-02 21:39:23 -0700

    added css styling for scrolly

---
+++ css/style.css
@@ -0,0 +1,100 @@
+.filler {
+  height: 20rem;
+  text-align: center;
+}
+#scrolly-side .scrolly {
+  display: flex;
+  max-width: 80rem;
+  margin: 3rem auto;
+  background-color: #6B7A8F;
+  padding: 1rem;
+}
+#scrolly-side .scrolly > * {
+  flex: 1;
+}
+#scrolly-side .scrolly article {
+  padding-right: 1rem;
+  max-width: 25rem;
+}
+#scrolly-side .scrolly article .step {
+  min-height: 67vh;
+  margin-bottom: 1rem;
+  background-color: #DCC7AA;
+  transition: background-color 250ms ease-in-out;
+}
+#scrolly-side .scrolly article .step:last-of-type {
+  margin-bottom: 0;
+}
+#scrolly-side .scrolly article .step.is-active {
+  background-color: #F7C331D9;
+}
+#scrolly-side .scrolly article .step p {
+  margin: 0;
+  padding: 1rem;
+  text-align: center;
+  font-weight: 400;
+  color: #f4f4f4;
+}
+#scrolly-side .scrolly figure.sticky {
+  position: -webkit-sticky;
+  position: sticky;
+  width: 100%;
+  height: 35vh;
+  background: #DCC7AA;
+  margin: 0;
+  top: 25vh;
+  left: 0;
+}
+#scrolly-side .scrolly figure.sticky .bar-outer {
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  -webkit-transform: translateY(-50%);
+          transform: translateY(-50%);
+  width: 90%;
+  height: 4rem;
+  background-color: #fff;
+}
+#scrolly-side .scrolly figure.sticky .bar-inner {
+  width: 10%;
+  height: 100%;
+  transition: width 1s ease-in-out;
+  background-color: #F7882F;
+}
+
+
+/*Added a footer*/
+footer {
+  position: sticky;
+  position: -webkit-sticky;
+  bottom: 0;
+  z-index: -1;
+  height: 50px;
+  background-color: dodgerblue;
+  color: white;
+  text-shadow: 2px 2px black;
+  padding: 5px 0;
+  text-align: center;
+}
+body {
+  background-image: linear-gradient(to top, transparent 60px, #fff 60px, #fff 0);
+  font: 14pt 'Crimson Text';
+  margin: 0;
+}
+html {
+  background-color: #fff;
+}
+main {
+  padding: 0px;
+  width: 75vw;
+  max-width: 600px;
+  margin: auto;
+}
+h1 {
+  color: #373D3F;
+}
+@media (max-width: 420px) {
+  footer {
+    font-size: 9pt;
+  }
+}
